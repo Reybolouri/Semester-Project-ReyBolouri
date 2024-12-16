0071bee112dcde24de8a66a817f97e1d8181bee3
RBolourui <160203017+RBolourui@users.noreply.github.com>
2024-12-16 13:37:36 -0600

    5

--- BLS_data_collection.py
+++ BLS_data_collection.py
@@ -9,25 +9,41 @@ import datetime
 URL = "https://api.bls.gov/publicAPI/v2/timeseries/data/"
 API_key = "2e94f53db1984894b301b64867ac70c0"
 
-seriesId = ['LNS12000000',#Civilian Employment (Seasonally Adjusted)
-            'LNS13000000',#Civilian Unemployment (Seasonally Adjusted)
+seriesId = ['LNS12000000',  #Civilian Employment (Seasonally Adjusted)
+            'LNS13000000', #Civilian Unemployment (Seasonally Adjusted)
             'LNS14000000', #Unemployment Rate (Seasonally Adjusted) 
             'CES0000000001', #Total Nonfarm Employment - Seasonally Adjusted
             'CES0500000002', # Total Private Average Weekly Hours of All Employees - Seasonally Adjusted
-            'CES0500000003']#Total Private Average Hourly Earnings of All Employees - Seasonally Adjusted
+            'CES0500000003']  #Total Private Average Hourly Earnings of All Employees - Seasonally Adjusted
 
-# Function for collecting  data
-def get_bls_data(seriesId, start_year, end_year):
+# Function for collecting  data - look at sample code again
+def collect_bls_data(seriesId, start_year, end_year):
     headers = {'Content-type': 'application/json'}
-    data = json.dumps({
-        "seriesId": seriesId,
-        "startyear": str(start_year),
-        "endyear": str(end_year),
-        "registrationkey": API_KEY
+    payload = json.dumps({
+        "seriesid": seriesId,
+        "startyear": "2019",
+        "endyear": str(datetime.now().year),
+        "registrationkey": API_key
     })
-    response = requests.post(URL, data=data, headers=headers)
+    response = requests.post(URL, data=payload, headers=headers)
+    response.raise_for_status() #covers HTTP errors
+    return response.json()
 
-    if response.status_code == 200:
-        return json.loads(response.text)
-    else:
-        raise Exception(f"Failed to collect data: {response.status_code}, {response.text}")
+def process_bls_data(json_data):
+    pr_data = []
+    for series in json_data['Results']['series']:
+        series_id = series['seriesID']
+        
+        for item in series['data']:
+            year = int(item['year'])
+            period = item['period']
+            value = float(item['value'])
+            pr_data.append({
+                "series_id": series_id,
+                "year": year,
+                "period": period,
+                "value": value,
+            })
+    df = pd.DataFrame(pr_data)
+    return df
+
