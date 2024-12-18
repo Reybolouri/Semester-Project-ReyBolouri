4ddf3d43fdd24dde8597a5458ea6a48d26541859
RBolourui <160203017+RBolourui@users.noreply.github.com>
2024-12-17 20:54:09 -0600

    Update BLS_app.py

--- BLS_app.py
+++ BLS_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+
 # Mapping of series IDs to human-readable names
 series_names = {
     "LNS12000000": "Civilian Employment",
