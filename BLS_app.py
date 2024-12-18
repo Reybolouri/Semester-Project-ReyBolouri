6b0e3342cb000a141fd40dd147d00175a1a39d1e
RBolourui <160203017+RBolourui@users.noreply.github.com>
2024-12-17 22:49:24 -0600

    Update BLS_app.py

--- BLS_app.py
+++ BLS_app.py
@@ -79,8 +79,8 @@ filtered_data = data[
 st.markdown(
     """
     <div style="text-align: center; padding: 10px 0;">
-        <h1 style="color:#0288D1;">US Labor Market</h1>
-        <p style="color:gray;">Unemployment and Labor trends over the years. Data sourced from the [Bureau of Labor Statistics](https://www.bls.gov/home.htm).</p>
+        <h1 style="color:#9C27B0;">US Labor Market</h1>
+        <p style="color:gray;">Unemployment and Labor trends over the years. Data sourced from the Bureau of Labor Statistics (BLS).</p>
     </div>
     """,
     unsafe_allow_html=True
